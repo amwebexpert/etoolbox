f7c68dcee0af72d0c34e8546d214ea849fc148b6
Andre Masson <andre.masson@survata.com>
2020-12-14 13:51:26 -0600

    Added home text

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: theme.spacing(0.1),
+        padding: theme.spacing(2),
+    },
+}));
+
 
 const Home: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            Welcome to the a collection of web developer utilities packaged as a desktop app!
-        </div>
+        <Paper className={classes.root}>
+            <h1>Web Toolbox</h1>
+            <p>Welcome to the a collection of web developer utilities packaged as a desktop app!</p>
+
+            <h2>Completed features</h2>
+            <ul>
+                <li>URL (and query string) Parser</li>
+                <li>JSON Formatter/Validator</li>
+            </ul>
+
+            <h2>Roadmap (list of items in the TODO list...)</h2>
+            <ul>
+                <li>URL Encoder/Decoder</li>
+                <li>Base64 Encoder/Decoder</li>
+                <li>Unix Time Converter</li>
+                <li>JWT Debugger</li>
+                <li>HTML Entity Encoder/Decoder</li>
+                <li>UUID Generator/Decoder</li>
+            </ul>
+        </Paper>
     );
 }
 
