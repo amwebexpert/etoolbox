620b12164d9e54b2b4480bd03d0eab9b414c25b6
Andre Masson <andre.masson@survata.com>
2020-12-15 13:01:49 -0600

    Base64 exception handler

--- src/components/Base64Encoder/services.ts
+++ src/components/Base64Encoder/services.ts
@@ -1,7 +1,11 @@
 export function transform(value: string, encoded: boolean) {
-    if (encoded) {
-        return btoa(value);
-    } else {
-        return atob(value);
+    try {
+        if (encoded) {
+            return btoa(value);
+        } else {
+            return atob(value);
+        }
+    } catch (e) {
+        return e.toString();
     }
 }
