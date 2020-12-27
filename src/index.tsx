d1bcaf7709001683a0535b5b567ba62f0042c63f
Andre Masson <andre.masson@survata.com>
2020-12-27 07:33:21 -0600

    Added context path

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ ReactDOM.render(
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <Router basename="/etoolbox">
+      <Router>
         <App />
       </Router>
     </ThemeProvider>
