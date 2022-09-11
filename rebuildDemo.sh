e8f7798444821265962bb729e148544d363d5f18
Andre Masson <amwebexpert@gmail.com>
2022-09-11 05:01:49 -0400

    docs: new online demo web release

--- rebuildDemo.sh
+++ rebuildDemo.sh
@@ -13,3 +13,8 @@ echo "\t --> [docs] folder re-created from build"
 echo "End of the build. Content of the [docs] folder:"
 echo "______________________________________________"
 ls -al docs/
+
+echo "adding (and pushing) new ./docs file changes to current branch"
+git add .
+git commit -m"docs: new online demo web release"
+git push
