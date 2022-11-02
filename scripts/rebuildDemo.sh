359c8710d581de0e3fa32f8aa2ba34a0bd50b08e
amwebexpert <amwebexpert@gmail.com>
2022-11-02 09:02:39 -0400

    docs: new online demo web release

--- scripts/rebuildDemo.sh
+++ scripts/rebuildDemo.sh
@@ -1,22 +1,30 @@
+echo "______________________________________________"
 echo "Building the app"
 rm -rf build
 npm run build
 
+echo "______________________________________________"
 echo "Removing the [docs] folder"
 rm -rf docs
 echo "\t --> [docs] folder removed"
 
+echo "______________________________________________"
 echo "Moving [build] folder to [docs] folder"
 mv build docs
 echo "\t --> [docs] folder re-created from build"
 
 echo "End of the build. Content of the [docs] folder:"
-echo "______________________________________________"
 ls -al docs/
 
-echo "adding (and pushing) new ./docs file changes to current branch"
+echo "______________________________________________"
+echo "adding new ./docs file changes to current branch"
 git add .
 git commit -m"docs: new online demo web release"
 
+echo "______________________________________________"
+echo "updating CHANGELOG.md, tagging this release, bumping version for next release"
 yarn run release
+
+echo "______________________________________________"
+echo "Pushing to Github (including tags)"
 git push --follow-tags
