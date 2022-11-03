ac5eae6ba803ed9d12ad63f1cdfa102c076ff623
amwebexpert <amwebexpert@gmail.com>
2022-11-03 09:28:14 -0400

    test:e2e: url encoder screen coverage

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable<Subject = any> {
+    /**
+     * Custom command to ... add your description here
+     * @example cy.clickOnMyJourneyInCandidateCabinet()
+     */
+    visitHomeScreen(): Chainable<null>;
+  }
+}
