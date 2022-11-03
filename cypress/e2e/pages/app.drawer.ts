886f33a9d5c875a1696164a5c08ca7182f5d22e3
André Masson <amwebexpert@gmail.com>
2022-11-03 08:50:22 -0400

    test:e2e URL Parser and encoding page coverage (#17)

--- cypress/e2e/pages/app.drawer.ts
+++ cypress/e2e/pages/app.drawer.ts
@@ -3,5 +3,7 @@ export const appDrawer = Object.freeze({
 
   getMenuItemHome: () => cy.findByRole('link', { name: 'Home' }),
 
+  getMenuItemURLEncode: () => cy.findByRole('link', { name: /^URL parsing/i }),
+
   getToggleMenu: () => cy.findByRole('button', { name: 'Toggle sidebar menu' }),
 });
