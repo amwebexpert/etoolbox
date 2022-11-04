1d5ab926ac1ee33e2699d00d6f418504f3d15e9f
amwebexpert <amwebexpert@gmail.com>
2022-11-04 15:13:52 -0400

    test:e2e added html tests report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Web Toolbox e2e tests report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
+    setupNodeEvents(on, _config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'http://localhost:3000',
   },
