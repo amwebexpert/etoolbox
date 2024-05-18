cee30d2971de0e76a9e285d7ec638e043921bb16
Andre-3rd-Viking <andre@t-b.ca>
2024-05-18 15:51:03 -0400

    fix: display version and date in about screen

--- src/components/About/AppDetail.tsx
+++ src/components/About/AppDetail.tsx
@@ -11,11 +11,12 @@ import TableContainer, { TableContainerProps } from '@mui/material/TableContaine
 import TableRow from '@mui/material/TableRow';
 import { withStyles } from '@mui/styles';
 
-import { getBuildUTCDate, getBuildUTCTimestamp } from '../../services/utils';
+import { APP_VERSION_INFO, LONG_VERSION_DATE } from '../../constants';
+import { getBuildUTCTimestamp } from '../../services/utils';
 import { useIsWidthUp } from '../../theme';
 import { useToasterUpdate } from '../Toaster/ToasterProvider';
 
-const StyledTableCell = withStyles(theme => ({
+const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
@@ -25,7 +26,7 @@ const StyledTableCell = withStyles(theme => ({
   },
 }))(TableCell);
 
-const StyledTableRow = withStyles(theme => ({
+const StyledTableRow = withStyles((theme) => ({
   root: {
     '&:nth-of-type(odd)': {
       backgroundColor: theme.palette.action.hover,
@@ -44,71 +45,72 @@ const AppDetail: React.FC<TableContainerProps> = ({ ...otherProps }) => {
 
   return (
     <TableContainer component={Paper} {...otherProps}>
-      <Table size={isMdUp ? 'medium' : 'small'} aria-label="about this application">
+      <Table size={isMdUp ? 'medium' : 'small'} aria-label='about this application'>
         <TableBody>
-          <StyledTableRow key="github">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='github'>
+            <StyledTableCell component='th' scope='row'>
               <GithubIcon />
             </StyledTableCell>
-            <StyledTableCell align="right">
-              <Link href="https://github.com/amwebexpert/etoolbox">GitHub project</Link>
+            <StyledTableCell align='right'>
+              <Link href='https://github.com/amwebexpert/etoolbox'>GitHub project</Link>
             </StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="privacy">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='privacy'>
+            <StyledTableCell component='th' scope='row'>
               <PrivacyIcon />
             </StyledTableCell>
-            <StyledTableCell align="right">
+            <StyledTableCell align='right'>
               <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://amwebexpert.github.io/etoolbox/privacy-policy.html">
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://amwebexpert.github.io/etoolbox/privacy-policy.html'
+              >
                 Privacy Policy
               </Link>
             </StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="versionNumber">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='versionNumber'>
+            <StyledTableCell component='th' scope='row'>
               Version
             </StyledTableCell>
-            <StyledTableCell align="right">{process.env.REACT_APP_VERSION}</StyledTableCell>
+            <StyledTableCell align='right'>{APP_VERSION_INFO.VERSION}</StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="versionDate">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='versionDate'>
+            <StyledTableCell component='th' scope='row'>
               Updated
             </StyledTableCell>
-            <StyledTableCell align="right" title={getBuildUTCTimestamp()} onClick={onBuildDateClick}>
-              {getBuildUTCDate()}
+            <StyledTableCell align='right' title={LONG_VERSION_DATE} onClick={onBuildDateClick}>
+              {APP_VERSION_INFO.VERSION_DATE}
             </StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="author">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='author'>
+            <StyledTableCell component='th' scope='row'>
               Author
             </StyledTableCell>
-            <StyledTableCell align="right">
-              <Link href="mailto:amwebexpert@gmail.com">André Masson</Link>
+            <StyledTableCell align='right'>
+              <Link href='mailto:amwebexpert@gmail.com'>André Masson</Link>
             </StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="iconMadeBy">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='iconMadeBy'>
+            <StyledTableCell component='th' scope='row'>
               Icon credits
             </StyledTableCell>
-            <StyledTableCell align="right">
-              <Link href="https://therealjerrylow.com/">Jerry Low</Link>
+            <StyledTableCell align='right'>
+              <Link href='https://therealjerrylow.com/'>Jerry Low</Link>
             </StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="reactVersiion">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='reactVersiion'>
+            <StyledTableCell component='th' scope='row'>
               React Version
             </StyledTableCell>
-            <StyledTableCell align="right">{React.version}</StyledTableCell>
+            <StyledTableCell align='right'>{React.version}</StyledTableCell>
           </StyledTableRow>
-          <StyledTableRow key="poweredByCRA">
-            <StyledTableCell component="th" scope="row">
+          <StyledTableRow key='poweredByCRA'>
+            <StyledTableCell component='th' scope='row'>
               Open source powered by
             </StyledTableCell>
-            <StyledTableCell align="right">
-              <Link href="https://reactjs.org/docs/create-a-new-react-app.html">Create React App</Link>
+            <StyledTableCell align='right'>
+              <Link href='https://reactjs.org/docs/create-a-new-react-app.html'>Create React App</Link>
             </StyledTableCell>
           </StyledTableRow>
         </TableBody>
