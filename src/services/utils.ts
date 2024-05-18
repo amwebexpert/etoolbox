8b82a208a7473da35bac02dfcb00c7b79eb5c6f5
Andre-3rd-Viking <andre@t-b.ca>
2024-05-18 14:43:17 -0400

    chore: remove wdyr and cleanup anoying console.error about defaultProps for mui icons

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -9,3 +9,12 @@ export function getBuildUTCTimestamp() {
 }
 
 export const IS_DEV_MODE: boolean = process.env.NODE_ENV === 'development';
+
+export const initConsole = () => {
+  // https://github.com/recharts/recharts/issues/3615#issuecomment-1636923358
+  const error = console.error;
+  console.error = (...args: any) => {
+    if (/defaultProps/.test(args[0])) return;
+    error(...args);
+  };
+};
