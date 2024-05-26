e737cebcaec10b2c7354360feff58de99deed2a6
Andre-3rd-Viking <andre@t-b.ca>
2024-05-26 12:35:02 -0400

    docs: new online demo web release

--- src/app-version-constants.ts
+++ src/app-version-constants.ts
@@ -5,8 +5,8 @@
 export const APP_VERSION_INFO = Object.freeze({
   NAME: 'web-toolbox',
   DESCRIPTION: "Collection of web developer utilities packaged as a desktop app",
-  VERSION: '2.5.0',
+  VERSION: '2.5.1',
   VERSION_DATE: '2024-05-26',
 });
 
-export const LONG_VERSION_DATE = '2.5.0 (2024-05-26)';
+export const LONG_VERSION_DATE = '2.5.1 (2024-05-26)';
