039ecf62c881d0a1c52d98086c853d9552b44892
Anthony Buchholz <anthony.buchholz@gmail.com>
2025-01-13 00:07:07 -0600

    feat: add minifier functionality
    Add in the minifier function and general formatJson function to the
    services.ts file for JSONFormatter.
    
    feat add minify button and functionality
    
    Add the minify button and connect it to the function to actually work as
    expected.

--- src/containers/JSONFormatter/services.ts
+++ src/containers/JSONFormatter/services.ts
@@ -2,23 +2,31 @@
 const replacer = (_key: string, value: any) =>
   value instanceof Object && !(value instanceof Array)
     ? Object.keys(value)
-        .sort()
-        .reduce((sorted: any, key: string) => {
-          sorted[key] = value[key];
-          return sorted;
-        }, {})
+      .sort()
+      .reduce((sorted: any, key: string) => {
+        sorted[key] = value[key];
+        return sorted;
+      }, {})
     : value;
 
-export function formatJson(value?: string): string {
+export function prettifyJson(value?: string): string {
+  return formatJson(4, value);
+}
+
+export function formatJson(space: number, value?: string): string {
   if (!value) {
     return '';
   }
 
   try {
     const obj = JSON.parse(value);
-    return JSON.stringify(obj, replacer, 4);
+    return JSON.stringify(obj, replacer, space);
   } catch (_e) {
     //  do nothing user may still be typing...
     return value;
   }
+}
+
+export function minifyJson(value?: string): string {
+  return formatJson(0, value)
 }
