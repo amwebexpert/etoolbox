52eca2cfcb16d062274bc991e1d411869a689894
André Masson <amwebexpert@gmail.com>
2026-01-19 11:00:28 -0600

    chore: index.html root element protection (#99)
    * refactor: index.html root elem protection
    
    * refactor: index.html root elem protection

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import "~/utils/node-polyfills";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Webapp } from "~/webapp";
-import "~/styles/global.css";
 
-createRoot(document.getElementById("root")!).render(
+import "~/styles/global.css";
+import { Webapp } from "~/webapp";
+
+const root = document.getElementById("root");
+if (!root) {
+  throw new Error("Root element not found within the DOM (double check your index.html file)");
+}
+
+createRoot(root).render(
   <StrictMode>
     <Webapp />
   </StrictMode>
