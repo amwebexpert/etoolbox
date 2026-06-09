9cc03db9a956177f1f75a15e9b0e722198790a37
André Masson <amwebexpert@gmail.com>
2026-06-09 09:02:12 -0400

    chore: replace yarn by bun

--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,9 @@ export const APP_VERSION_INFO = Object.freeze({
   DESCRIPTION: "Collection of web development utilities. Like a Swiss knife for web developers.",
   REPOSITORY: "https://github.com/amwebexpert/etoolbox",
   AUTHOR: "amwebexpert@gmail.com",
-  VERSION: "4.6.0",
-  VERSION_DATE: "2026-06-08",
-  VERSION_DATE_ISO: "2026-06-08T09:38:36.180Z",
+  VERSION: "4.6.1",
+  VERSION_DATE: "2026-06-09",
+  VERSION_DATE_ISO: "2026-06-09T13:01:44.529Z",
 });
 
-export const LONG_VERSION_DATE = "4.6.0 (2026-06-08)";
+export const LONG_VERSION_DATE = "4.6.1 (2026-06-09)";
